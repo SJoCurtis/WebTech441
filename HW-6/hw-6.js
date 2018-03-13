caa74fcffa988d3558ba0a78e11b68f56e9388eb
JoJo <inputoutput@wl-186-176.wireless.umt.edu>
2018-03-13 11:41:19 -0600

    mmm
    mnnjn

--- HW-6/hw-6.js
+++ HW-6/hw-6.js
@@ -39,48 +39,48 @@ body.style.margin="40px";
 body.style.backgroundImage = 'url(http://blog.codegrape.com/wp-content/uploads/2017/09/revamp-website-2.gif)';
 //Element Type
 let the = document.createElement("div");
-let theChild = document.createElement("h1");
-let theChild2 = document.createElement('p');
-let theChild3 = document.createElement('P');
-let theChild4 = document.createElement('h2');
-let theChild5 = document.createElement('p');
-let theChild6 = document.createElement('p');
-let theChild7 = document.createElement('a');
-let theChild8 = document.createElement('a');
+let theBabe = document.createElement("h1");
+let theBabe2 = document.createElement('p');
+let theBabe3 = document.createElement('P');
+let theBabe4 = document.createElement('h2');
+let theBabe5 = document.createElement('p');
+let theBabe6 = document.createElement('p');
+let theBabe7 = document.createElement('a');
+let theBabe8 = document.createElement('a');
 //Element Content
-theChild.innerText = "☁ ☁ ☁ ☁ I LOVE RAINBOWS ☁ ☁ ☁ ☁";
-theChild2.innerText="A rainbow is a bow or arc of prismatic colors appearing in the heavens opposite the sun and caused by the refraction and reflection of the sun's rays in drops of rain.";
-theChild3.innerText="The rainbow represent happiness and joy. It can be elusive. But sometimes it requires a storm to enjoy a rainbow. The rainbow and happiness require something first.";
-theChild4.innerText="RAINBOW POSITIVITY"
-theChild5.innerText="If life is blue, just select another color from the rainbow. - Unknown"
-theChild6.innerText="The true harvest of my daily life is somewhat as intangible and indescribable as the tints of morning or evening. It is a little star dust caught, a segment of the rainbow which I have clutched. - Henry David Thoreau"
-theChild7.innerText="Learn About Rainbow Brite";
-theChild8.innerText="More Rainbow Positivity"
+theBabe.innerText = "☁ ☁ ☁ ☁ I LOVE RAINBOWS ☁ ☁ ☁ ☁";
+theBabe2.innerText="A rainbow is a bow or arc of prismatic colors appearing in the heavens opposite the sun and caused by the refraction and reflection of the sun's rays in drops of rain.";
+theBabe3.innerText="The rainbow represent happiness and joy. It can be elusive. But sometimes it requires a storm to enjoy a rainbow. The rainbow and happiness require something first.";
+theBabe4.innerText="RAINBOW POSITIVITY"
+theBabe5.innerText="If life is blue, just select another color from the rainbow. - Unknown"
+theBabe6.innerText="The true harvest of my daily life is somewhat as intangible and indescribable as the tints of morning or evening. It is a little star dust caught, a segment of the rainbow which I have clutched. - Henry David Thoreau"
+theBabe7.innerText="Learn About Rainbow Brite";
+theBabe8.innerText="More Rainbow Positivity"
 //Styling
-theChild.setAttribute("style","padding:2em; margin:0; font-family:; font-weight:bold; text-align:center; color:#FF3232");
-theChild2.setAttribute("style","padding:0em; margin:0; font-weight:bold; text-align:center; color:#202020");
-theChild3.setAttribute("style","padding:1em; margin:0;  font-weight:bold; text-align:center; color:#202020");
-theChild4.setAttribute("style","padding:1em; margin:0;  font-weight:bold; text-align:center; color:#00008b");
-theChild5.setAttribute("style","padding:0em; margin:0; font-weight:bold; font-style:italic; text-align:center; color:#202020");
-theChild6.setAttribute("style","padding:0em; margin:0; font-weight:bold; font-style:italic; text-align:center; color:#202020");
-theChild7.setAttribute("style","padding:0em; margin:530px; color:#FF69B4");
-theChild7.setAttribute("href","https://www.huffingtonpost.com/kenyth-mogan/the-colorful-history-of-r_b_10345612.html");
-theChild8.setAttribute("style","padding:0em; margin:530px; color:#885EAD");
-theChild8.setAttribute("href","http://www.quotesoup.com/quotes/rainbow");
+theBabe.setAttribute("style","padding:2em; margin:0; font-family:; font-weight:bold; text-align:center; color:#FF3232");
+theBabe2.setAttribute("style","padding:0em; margin:0; font-weight:bold; text-align:center; color:#202020");
+theBabe3.setAttribute("style","padding:1em; margin:0;  font-weight:bold; text-align:center; color:#202020");
+theBabe4.setAttribute("style","padding:1em; margin:0;  font-weight:bold; text-align:center; color:#00008b");
+theBabe5.setAttribute("style","padding:0em; margin:0; font-weight:bold; font-style:italic; text-align:center; color:#202020");
+theBabe6.setAttribute("style","padding:0em; margin:0; font-weight:bold; font-style:italic; text-align:center; color:#202020");
+theBabe7.setAttribute("style","padding:0em; margin:530px; color:#FF69B4");
+theBabe7.setAttribute("href","https://www.huffingtonpost.com/kenyth-mogan/the-colorful-history-of-r_b_10345612.html");
+theBabe8.setAttribute("style","padding:0em; margin:530px; color:#885EAD");
+theBabe8.setAttribute("href","http://www.quotesoup.com/quotes/rainbow");
 //Image Styling
 myImage.setAttribute("style","padding:20px; margin:340px");
 myImage1.setAttribute("style","padding:20px; margin:340px");
 //Positions
-the.appendChild(theChild);
+the.appendChild(theBabe);
 the.appendChild(myImage);
-the.appendChild(theChild2);
-the.appendChild(theChild3);
-the.appendChild(theChild4);
-the.appendChild(theChild5);
-the.appendChild(theChild6);
-the.appendChild(theChild8);
+the.appendChild(theBabe2);
+the.appendChild(theBabe3);
+the.appendChild(theBabe4);
+the.appendChild(theBabe5);
+the.appendChild(theBabe6);
+the.appendChild(theBabe8);
 the.appendChild(myImage1);
-the.appendChild(theChild7);
+the.appendChild(theBabe7);
 
 body.appendChild(the);
 //The End
