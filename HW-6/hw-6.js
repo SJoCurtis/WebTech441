35a67061a25947705f30ab86a0f00eac6764478a
JoJo <inputoutput@wl-186-176.wireless.umt.edu>
2018-03-13 10:42:05 -0600

    jnjjn

--- HW-6/hw-6.js
+++ HW-6/hw-6.js
@@ -48,14 +48,14 @@ let theChild6 = document.createElement('p');
 let theChild7 = document.createElement('a');
 theChild.innerText = "☁ ☁ ☁ ☁ I LOVE RAINBOWS ☁ ☁ ☁ ☁";
 theChild2.innerText="A rainbow is a bow or arc of prismatic colors appearing in the heavens opposite the sun and caused by the refraction and reflection of the sun's rays in drops of rain.";
-theChild3.innerText="Rainbow represent happiness and joy. It can be elusive. But sometimes it requires a storm to enjoy a rainbow. The rainbow and happiness require something first.";
+theChild3.innerText="The rainbow represent happiness and joy. It can be elusive. But sometimes it requires a storm to enjoy a rainbow. The rainbow and happiness require something first.";
 theChild4.innerText="RAINBOW POSITIVITY"
 theChild5.innerText="If life is blue, just select another color from the rainbow. - Unknown"
 theChild6.innerText="The true harvest of my daily life is somewhat as intangible and indescribable as the tints of morning or evening. It is a little star dust caught, a segment of the rainbow which I have clutched. - Henry David Thoreau"
 theChild7.innerText="Learn About Rainbow Brite";
 theChild.setAttribute("style","padding:2em; margin:0; font-family:; font-weight:bold; text-align:center; color:#FF3232");
 theChild2.setAttribute("style","padding:0em; margin:0; font-style:bold; text-align:left; color:#202020");
-theChild3.setAttribute("style","padding:1em; margin:0;  font-weight:bold; text-align:left; color:#202020");
+theChild3.setAttribute("style","padding:1em; margin:0;  font-weight:; text-align:left; color:#202020");
 theChild4.setAttribute("style","padding:1em; margin:0;  font-weight:bold; text-align:center; color:#00008b");
 theChild5.setAttribute("style","padding:0em; margin:0;  font-style:italic; text-align:center; color:#202020");
 theChild6.setAttribute("style","padding:0em; margin:0;  font-style:italic; text-align:center; color:#202020");
