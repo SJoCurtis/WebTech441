a92a11482c9c9d92cd90c6e49fde215b5dfb950e
JoJo <inputoutput@wl-186-176.wireless.umt.edu>
2018-03-13 10:26:11 -0600

    asd

--- HW-6/hw-6.js
+++ HW-6/hw-6.js
@@ -35,7 +35,7 @@ let body = document.body;
 body.innerText = "";
 
 body.style.padding = "10px";
-body.style.margin="10px";
+body.style.margin="60px";
 body.style.backgroundImage = 'url(http://blog.codegrape.com/wp-content/uploads/2017/09/revamp-website-2.gif)';
 
 let the = document.createElement("div");
