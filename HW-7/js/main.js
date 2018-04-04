73810e8dd83230fc7ae9c3a748f2d91400331611
JoJo <inputoutput@wl-187-198.wireless.umt.edu>
2018-04-04 16:40:40 -0600

    dffng

--- HW-7/js/main.js
+++ HW-7/js/main.js
@@ -1,2 +1,9 @@
-confirm("Would You Like To Continue");
+var myAge = prompt("What is your age?");
+if(myAge>=14){
+  confirm("Play On");
+}
+else{
+  alert("Play With Caution");
+}
+confirm("Would You Like To Continue?");
 var age = prompt("What's Your Name?");
