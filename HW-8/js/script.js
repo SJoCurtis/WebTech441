8ffaa21a439836d8da4b8d5dc8b37a7c998440ca
JoJo <inputoutput@wl-186-249.wireless.umt.edu>
2018-04-25 13:29:43 -0600

    yup

--- HW-8/js/script.js
+++ HW-8/js/script.js
@@ -1,3 +1,4 @@
+//Function that allows the loading into an iframe.
 function inIframe() {
     try {
         return window.self !== window.top;
@@ -5,15 +6,15 @@ function inIframe() {
         return true;
     }
 }
-
+//Variable for alternating backgroun colors.
 var colors = ['#ffffe6', '#f2ffe6', '#e6ffe6', ' #e6ffff', '#e6ffff', '#e6e6ff', '#ecf9f2', '#fff5e6', "#ffffcc", "#cceeff", "#f2d9e6", "#f0f0f5"];
 var currentQuote = '',
     currentAuthor = '';
-
+// Function responisble for the window that opens with the tumblr url.
 function openURL(url) {
     window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
 }
-
+// Retrieves the suprise/random quote from mashape.com in JSON format and contains the tumblr widget, and opacity, etc.
 function getQuote() {
     $.ajax({
         headers: {
@@ -50,7 +51,7 @@ function getQuote() {
                     }, 500);
                     $('#author').html(r.author);
                 });
-
+// This is the math controlling the "random" color background changes.
             var color = Math.floor(Math.random() * colors.length);
             $("html body").animate({
                 backgroundColor: colors[color],
@@ -62,14 +63,10 @@ function getQuote() {
         }
     });
 }
+// Clicky click.
 $(document).ready(function() {
     getQuote();
     $('#new-quote').on('click', getQuote);
-    $('#tweet-quote').on('click', function() {
-        if (!inIframe()) {
-            openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-        }
-    });
     $('#tumblr-quote').on('click', function() {
         if (!inIframe()) {
             openURL('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' + encodeURIComponent(currentAuthor) + '&content=' + encodeURIComponent(currentQuote) + '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
